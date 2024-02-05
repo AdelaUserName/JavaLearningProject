315ac6a0f2909839419232eff5443404d28972f4
AdelaUserName <adela.starciuc1@gmail.com>
2024-02-05 16:33:35 +0200

    added the variables and comments task

---
+++ src/main/java/com/AdelaQA/variablesandcomments/VariablesAndComments.java
@@ -0,0 +1,50 @@
+package com.AdelaQA.variablesandcomments;
+
+public class VariablesAndComments {
+    public static void main(String[] args) {
+        // Declare the primitive data type variables
+
+        boolean falseBooleanVariable;
+        boolean trueBooleanVariable;
+        char firstCharFromAscii;
+        char lastCharFromAscii;
+        byte lowerLimitOfByteDataType;
+        byte upperLimitOfByteDataType;
+        short lowerLimitOfShortDataType;
+        short upperLimitOfShortDataType;
+        int lowerLimitOfIntDataType;
+        int upperLimitOfIntDataType;
+        long lowerLimitOfLongDataType;
+        long upperLimitOfLongDataType;
+        float lowerLimitOfFloatDataType;
+        float upperLimitOfFloatDataType;
+        double lowerLimitOfDoubleDataType;
+        double upperLimitOfDoubleDataType;
+
+        // Initialization of the above variables
+
+        falseBooleanVariable = false;
+        trueBooleanVariable = true;
+        firstCharFromAscii = 97;
+        lastCharFromAscii = 'z';
+
+        // Ascii Characters mai multe detalii caut pe net, a coincide cu 97 in tabelul Ascii.
+
+        lowerLimitOfByteDataType = -128;
+        upperLimitOfByteDataType = 127;
+        lowerLimitOfShortDataType = -32768;
+        upperLimitOfShortDataType = 32767;
+        lowerLimitOfIntDataType = -2147483648;
+        upperLimitOfIntDataType = 2147483647;
+        lowerLimitOfLongDataType = -9223372036854775808L;
+        upperLimitOfLongDataType = 922337203685775808L;
+        lowerLimitOfFloatDataType = -3.4E+38f;
+        upperLimitOfFloatDataType = 3.4E+38f;
+        lowerLimitOfDoubleDataType = -1.7E+308d;
+        upperLimitOfDoubleDataType = 1.7E+308d;
+
+        System.out.println(firstCharFromAscii);
+    }
+
+
+}
